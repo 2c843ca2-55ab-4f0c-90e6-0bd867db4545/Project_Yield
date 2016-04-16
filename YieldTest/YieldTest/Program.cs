c79800d5a1fa3c59b36da6fadeefc65b9738569f
2c843ca2-55ab-4f0c-90e6-0bd867db4545 <chenakeshava1@gmail.com>
2016-04-16 21:39:39 +0530

    Initial Commit

---
+++ YieldTest/YieldTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YieldTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbl = 1.To(10).Multiply(2);
+
+            foreach (var i in dbl)
+                Console.WriteLine(i);
+
+            Console.ReadLine();
+        }
+    }
+
+    static class yieldTest
+    {
+        public static IEnumerable<int> Multiply(this IEnumerable<int> value, int multiplier)
+        {
+            foreach (int v in value)
+                yield return v * multiplier;
+        }
+
+        public static IEnumerable<int> To(this int value, int inclusiveMax)
+        {
+            for (int i = value; i <= inclusiveMax; i++)
+                yield return i;
+        }
+    }
+}
